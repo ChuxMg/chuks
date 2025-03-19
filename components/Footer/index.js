b1a8e15cb615d0061a0b050af1d2c4de0a4b83b2
ChuxMg <chuxmgbojikwe@gmail.com>
2025-03-19 19:23:37 +0700

    Implement upgrade

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -28,8 +28,8 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         © {currentYear} All Rights Reserved{" "}
-        <Link href="/">
-          <a className="underline underline-offset-1">Chuks Mgbojikwe</a>
+        <Link href="/" className="underline underline-offset-1">
+          Chuks Mgbojikwe
         </Link>
       </h1>
     </>
