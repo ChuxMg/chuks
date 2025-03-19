3d3c7881bad50155f4ee5784324aa48c37c48444
ChuxMg <chuxmgbojikwe@gmail.com>
2025-03-19 16:25:19 +0700

    Implement update

---
+++ components/Footer/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Socials from "../Socials";
+import Link from "next/link";
+import Button from "../Button";
+
+const Footer = ({}) => {
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <>
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+        <div>
+          <h1 className="text-2xl text-bold">Contact.</h1>
+          <div className="mt-10">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              LET&apos;S BUILD GREAT THINGS
+            </h1>
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              TOGETHER
+            </h1>
+            <Button type="primary">Schedule a call</Button>
+            <div className="mt-10">
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+        © {currentYear} All Rights Reserved{" "}
+        <Link href="/">
+          <a className="underline underline-offset-1">Chuks Mgbojikwe</a>
+        </Link>
+      </h1>
+    </>
+  );
+};
+
+export default Footer;
